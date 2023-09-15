0b124392ec66d767ab64c233880b31b3b3d0fae7
kagarg <ivan.kurdukov@gmail.com>
2023-09-15 20:55:25 +0700

    fixed density_calc function

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,16 @@
 
 int main() {
 	setlocale(LC_ALL, "ru");
-	std::cout << "Введите v, (v > 0) >";
+	/*std::cout << "Введите v, (v > 0) >";
 	double v;
 	std::cin >> v;
 	if (v <= 0) { std::cout << "Ошибка ввода"; exit(-1); }
 	std::cout << "Дисперсия равна " << Dispersion_calc(v) << std::endl << "Коэф. эксцесса равен " << Excess_calc(v) << std::endl
 			  << "Плотность распределения равна " << Density_calc(0,v) << std::endl;
+	std::cout << "";*/
+	std::vector<double>vec = Mixed_Func_calc(0.5, 0,1.5,3,2, 0,1.5,3,2);
+	for (double i = 0; i < vec.size(); i++) {
+		std::cout << vec[i]<< " ";
+	}
+	
 }
--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,14 @@
 
 
 double Density_calc(double x, double v) {
-	double density = (v / (2 * tgamma(1 / v)) * exp(pow(-abs(x), v)));
+	/*double density = (v / (2 * tgamma(1 / v)) * exp(pow(-abs(x), v)));*/
+	double density = (v / (2 * tgamma(1 / v)) * exp(exp(v * log(-abs(x)))));
 	return density;
 }
 
 double Modified_Density_calc(double x, double v, double u, double l) {
-	double mod_density = (v / (2 * tgamma(1 / v)) * exp(pow(-abs((x - u) / l), v))) / l;
+	//double mod_density = (v / (2 * tgamma(1 / v)) * exp(pow(-abs((x - u) / l), v))) / l;
+	double mod_density = (v / (2 * tgamma(1 / v)) * exp(exp(v*log(-abs(x))))) / l;
 	return mod_density;
 }
 double Expected_value() { return 0; }
@@ -44,7 +46,7 @@ std::vector<double> Mixed_Func_calc(double p, double x1, double v1, double u1, d
 	std::vector<double>results1 = Modified_Func_calc(x1, v1, u1, l1);
 	std::vector<double>results2 = Modified_Func_calc(x2, v2, u2, l2);
 	double mixed_density = ((1 - p) * results1[2]) + (p * results2[2]);
-	double mixed_expected_value = ((1 - p) * results1[3]) + (p * results2[3]);
+	double mixed_expected_value = /*((1 - p) * results1[3]) + (p * results2[3]);*/ ((1 - p) * Modified_Density_calc(x1, v1, u1, l1) + p * Modified_Density_calc(x2, v2, u2, l2));
 	double mixed_dispersion = (1 - p) * (pow(results1[3], 2) + results1[0]) - pow(mixed_expected_value, 2) + (p * (pow(results2[3], 2) + results2[0]) - pow(mixed_expected_value, 2));
 	double asymmetry = (1 / (pow(mixed_dispersion, 3 / 2))) * ((1 - p) * (pow(results1[3] - mixed_expected_value, 3) + 3 * (results1[3] - mixed_expected_value) * results1[0] + pow(results1[0], 3 / 2) * Asymmetry_calc()) + p * (pow(results2[3] - mixed_expected_value, 3) + 3 * (results2[3] - mixed_expected_value) * results2[0] + pow(results2[0], 3 / 2) * Asymmetry_calc()));
 	double excess = (1 / pow(mixed_dispersion, 3)) * ((1 - p) * (pow(results1[3] - mixed_expected_value, 4) + 6 * pow(results1[3] - mixed_expected_value, 2) * results1[0] + 4 * (results1[3] - mixed_expected_value) * pow(results1[0], 3 / 2) * Asymmetry_calc() + pow(results1[0], 2) * Excess_calc(v1)) - 3 + p * ((pow(results2[3] - mixed_expected_value, 4) + 6 * pow(results2[3] - mixed_expected_value, 2) * results2[0] + 4 * (results2[3] - mixed_expected_value) * pow(results2[0], 3 / 2) * Asymmetry_calc() + pow(results2[0], 2) * Excess_calc(v2))) - 3);
