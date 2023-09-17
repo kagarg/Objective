136ca682753de88a1c92dc3f12f310958062780e
kagarg <ivan.kurdukov@gmail.com>
2023-09-17 20:31:44 +0700

    increased code quality

--- Main.cpp
+++ Main.cpp
@@ -11,17 +11,18 @@ int main() {
 	// standart function print
 	std::cout << "Дисперсия равна " << Dispersion_calc(v) << std::endl << "Коэф. эксцесса равен " << Excess_calc(v) << std::endl
 			  << "Плотность распределения равна " << Density_calc(0,v) << std::endl;
-	
 
-	// random value modeling print
+	// random value modeling print for standart func
 	std::cout << std::endl << "Случайная величина для стандартного распределения > " << Random_item(v)<<std::endl;
+	
+	//mixed function print
 	std::cout << "Введите параметр смеси р > ";
 	double p;
 	std::cin >> p;
 	std::cout << "Введите параметр v для второго распределения (для смеси) > ";
 	double v2;
 	std::cin >> v2;
-	//mixed function print
+	
 	std::vector<double>vec = Mixed_Func_calc(p, 0,v,3,2, 0,v2,3,2);
 	std::cout << std::endl << "Параметры смеси распределений : " << std::endl;
 	std::cout << "dispersion " << vec[0] << std::endl << "excess "<<vec[1]<< std::endl<< "density " << vec[2] << std::endl << "expected value " << vec[3] << std::endl << "asymmetry " << vec[4] << std::endl;
