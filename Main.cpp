f5bfe0311933f41e89615c0949a98eb33a171325
kagarg <ivan.kurdukov@gmail.com>
2023-09-17 21:13:56 +0700

    updated data insert

--- Main.cpp
+++ Main.cpp
@@ -4,10 +4,30 @@
 int main() {
 	setlocale(LC_ALL, "ru");
 	srand((unsigned)time(0));
-	std::cout << "Введите v, (v > 0) >";
-	double v;
-	std::cin >> v;
-	if (v <= 0) { std::cout << "Ошибка ввода"; exit(-1); }
+
+	std::cout << "Введите v (v > 0), p (0 < p < 1), v2 (v2 > 0), l1, l2, u1, u2 > ";
+	double v, l1, l2, u1, u2, p, v2;
+	std::cin >> v >> p >> v2 >> l1 >> l2 >> u1 >> u2;
+
+	while (std::cin.fail()) {							// проверка вводимых данных
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Ошибка ввода. Введите значения повторно > ";
+		std::cin >> p >> v2 >> l1 >> l2 >> u1 >> u2;
+	}
+	while (v <= 0) {
+		std::cout << "Ошибка ввода v. Введите значение повтороно > ";
+		std::cin >> v;
+	}
+	while (v2 <= 0) {
+		std::cout << "Ошибка в v2. Введите значение больше нуля > ";
+		std::cin >> v2;
+	}
+	while ((p >= 1)||(p <=0)) {
+		std::cout << "Ошибка в p. Введите значение повтороно > ";
+		std::cin >> p;
+	}													//
+	
 	// standart function print
 	std::cout << "Дисперсия равна " << Dispersion_calc(v) << std::endl << "Коэф. эксцесса равен " << Excess_calc(v) << std::endl
 			  << "Плотность распределения равна " << Density_calc(0,v) << std::endl;
@@ -16,14 +36,7 @@ int main() {
 	std::cout << std::endl << "Случайная величина для стандартного распределения > " << Random_item(v)<<std::endl;
 	
 	//mixed function print
-	std::cout << "Введите параметр смеси р > ";
-	double p;
-	std::cin >> p;
-	std::cout << "Введите параметр v для второго распределения (для смеси) > ";
-	double v2;
-	std::cin >> v2;
-	
-	std::vector<double>vec = Mixed_Func_calc(p, 0,v,3,2, 0,v2,3,2);
+	std::vector<double>vec = Mixed_Func_calc(p, 0,v,u1,l1, 0,v2,u2,l2);
 	std::cout << std::endl << "Параметры смеси распределений : " << std::endl;
 	std::cout << "dispersion " << vec[0] << std::endl << "excess "<<vec[1]<< std::endl<< "density " << vec[2] << std::endl << "expected value " << vec[3] << std::endl << "asymmetry " << vec[4] << std::endl;
 	
