c7ea2a4ba462ed884bdd59142b1c2fa19c8f8005
kagarg <ivan.kurdukov@gmail.com>
2023-09-17 19:24:38 +0700

    updated ui

--- Main.cpp
+++ Main.cpp
@@ -4,16 +4,20 @@
 int main() {
 	setlocale(LC_ALL, "ru");
 	srand((unsigned)time(0));
-	/*std::cout << "Введите v, (v > 0) >";
+	std::cout << "Введите v, (v > 0) >";
 	double v;
 	std::cin >> v;
 	if (v <= 0) { std::cout << "Ошибка ввода"; exit(-1); }
+	// standart function print
 	std::cout << "Дисперсия равна " << Dispersion_calc(v) << std::endl << "Коэф. эксцесса равен " << Excess_calc(v) << std::endl
 			  << "Плотность распределения равна " << Density_calc(0,v) << std::endl;
-	std::cout << "";*/
+	std::cout << std::endl << "Величины для смешанного распределения: " << std::endl;
+	//mixed function print
 	std::vector<double>vec = Mixed_Func_calc(0.5, 0,1.5,3,2, 0,1.5,3,2);
-	for (int i = 0; i < vec.size(); i++) {
-		std::cout << vec[i]<< " ";
-	}
-	std::cout <<std::endl<<Random_item2(1);
+	std::cout << "dispersion " << vec[0] << std::endl << "excess "<<vec[1]<< std::endl<< "density " << vec[2] << std::endl << "expected value " << vec[3] << std::endl << "asymmetry " << vec[4] << std::endl;
+	// random value modeling print
+	if (1 <= v < 2){ std::cout << std::endl <<"Случайная величина > " << Random_item12(v); }
+	else if (v >= 2) { std::cout << std::endl << "Случайная величина > " << Random_item2(v); }
+
+	
 }
